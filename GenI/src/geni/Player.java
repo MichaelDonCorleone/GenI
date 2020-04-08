606eea091dad1e6640f1a2bff1d1b5ac94dbf9d6
Michael Corleone <32565641+MichaelDonCorleone@users.noreply.github.com>
2020-04-08 12:55:03 -0600

    Add files via upload

--- GenI/src/geni/Player.java
+++ GenI/src/geni/Player.java
@@ -63,8 +63,49 @@ public class Player {
         }
     }
     
-    public void setLineUp(int position, Pokemon pokemonToBeAssigned) {
-        
+    public void setLineUp(int positionToBeAssignedTo, Pokemon pokemonToBeAssigned) {
+        boolean alreadyInLineUp = false;
+        int positionInLineUp = -1;
+        int pokemonIDtoSwitch = -1;
+        for(int i =0; i < 6; i++) {
+            //check if position iterated is empty
+            if(lineup[i] != null) {
+                if(lineup[i].getID() == pokemonToBeAssigned.getID()) {
+                    alreadyInLineUp = true;
+                    positionInLineUp = i;
+                }
+            } else {
+                if(alreadyInLineUp == false) {
+                    lineup[i] = pokemonToBeAssigned;
+                    break;
+                } else {
+                    //check if we are trying to assign our pokemon that is already in the lineup
+                    //to a null position
+                    if(positionToBeAssignedTo >= i) {
+                        break;
+                    } else {
+                        //check if we are trying to assign the pokemon to the position it already is.
+                        if(lineup[positionToBeAssignedTo].getID() == pokemonToBeAssigned.getID()) {
+                            break;
+                        } else {
+                            lineup[positionInLineUp] = lineup[positionToBeAssignedTo];
+                            lineup[positionToBeAssignedTo] = pokemonToBeAssigned;
+                        }
+                    }
+                }
+            }
+            if(i==5) {
+                if(positionInLineUp == -1) {
+                    lineup[positionToBeAssignedTo] = pokemonToBeAssigned;
+                } else {
+                    //take the pokemon from the position that we want to swap this pokemon to
+                    //and transfer it to the position the pokemon we want to swap from is 
+                    //then assign the pokemon to the positionToBeAssignedTo.
+                    lineup[positionInLineUp] = lineup[positionToBeAssignedTo];
+                    lineup[positionToBeAssignedTo] = pokemonToBeAssigned;
+                }
+            }
+        }
     }
     
     //getters
